4ded5b37df13ec57d9dfedde2c614b1fd65e630c
u10914226 <u10914226@ms.ttu.edu.tw>
2023-03-14 20:43:02 +0800

    Player movement script but the jump part hasn't done.

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    private PlayerInput playerInput;
+    private InputAction Move;
+    private InputAction Jump;
+    private Rigidbody2D playerRig;
+    private Transform groundDetector;
+    [SerializeField]
+    private float movenSpeed;
+    [SerializeField]
+    private float jumpForce;
+    private bool grounded;
+
+
+    private bool jumpAble;
+
+    private void Awake()
+    {
+        groundDetector = transform.Find("Misc/GroundDetector");
+        playerRig = transform.Find("Model").gameObject.GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        Move = playerInput.actions["Move"];
+        Jump = playerInput.actions["Jump"];
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        jumpAble = true;
     }
-
+    public void OnWASD()
+    {
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        //grounded = Physics2D.Raycast( Vector2.down, movenSpeed, LayerMask.GetMask("Ground"))
+        if (Move.ReadValue<Vector2>().x > 0)
+        {
+            transform.Translate(movenSpeed * Time.deltaTime, 0, 0);
+            
+        }
+        else if(Move.ReadValue<Vector2>().x < 0)
+        {
+            transform.Translate(-movenSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Jump.IsPressed() && jumpAble)
+        {
+            jumpAble = false;
+            playerRig.velocity = new Vector2(0, jumpForce);
+        }
     }
 }
