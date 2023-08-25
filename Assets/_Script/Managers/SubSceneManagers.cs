2a87494179ce16382f0e1d5adc1141a768cdf1e6
u10914226 <u10914226@ms.ttu.edu.tw>
2023-08-25 14:55:36 +0800

    存檔系統修正

---
+++ Assets/_Script/Managers/SubSceneManagers.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Eflatun.SceneReference;
+
+public class SubSceneManagers : MonoBehaviour
+{
+    private void Awake()
+    {
+
+    }
+}
--- Assets/_Script/SaveSystem/DataPersistenceManager.cs
+++ Assets/_Script/SaveSystem/DataPersistenceManager.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+using UnityEngine.SceneManagement;
 public class DataPersistenceManager : MonoBehaviour
 {
-
     [SerializeField] private string fileName;
     [SerializeField] private bool useEncryption = false;
 
@@ -28,15 +27,40 @@ public class DataPersistenceManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+
+        dataHandler = new FileDataHandler(Application.persistentDataPath, fileName, useEncryption);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
     }
 
     private void Start()
     {
-        dataHandler = new FileDataHandler(Application.persistentDataPath, fileName, useEncryption);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Loaded scene: " + scene.name);
+
         dataPersistanceObjects = FindAllDataPersistenceObjects();
         LoadGame();
     }
 
+    private void OnSceneUnloaded(Scene scene)
+    {
+        Debug.Log("Unloaded scene: " + scene.name);
+
+        SaveGame();
+    }
     private List<IDataPersistance> FindAllDataPersistenceObjects()
     {
         IEnumerable<IDataPersistance> dataPersistanceObjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistance>();
