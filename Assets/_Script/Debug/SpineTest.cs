381ead52d71cadbdc80e496a70b97591c95613bd
謝清華 <uni.toberuneko@gmail.com>
2023-09-01 00:19:59 +0800

    spine測試

---
+++ Assets/_Script/Debug/SpineTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpineTest : MonoBehaviour
+{
+    public void OnTestEvent()
+    {
+        Debug.Log("Test!");
+    }
+}
