97fb116bcd33b70a377b9cefe32b1655c9506796
MSI\User <autumnmoon910228@gmail.com>
2023-09-15 01:40:56 +0800

    透視試作

--- Assets/Tiled/Tileset/Level1 Tileset.tsx
+++ Assets/Tiled/Tileset/Level1 Tileset.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="Level1 Tileset" tilewidth="256" tileheight="256" tilecount="56" columns="8" tilerendersize="grid">
  <grid orientation="orthogonal" width="16" height="16"/>
- <image source="../../2D/TileSets/NewTiles1.png" trans="9d9d9d" width="2048" height="1792"/>
+ <image source="../../2D/TileSets/0828 tiles.png" trans="9d9d9d" width="2048" height="1792"/>
  <tile id="0">
   <properties>
    <property name="unity:layer" value="Ground"/>
