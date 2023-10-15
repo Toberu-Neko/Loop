51a7d15bc8a47e49610184547ae997c49ad83fa6
謝清華 <uni.toberuneko@gmail.com>
2023-10-15 17:55:07 +0800

    鏡頭設定

--- Assets/_Script/Map/MapCam/BossRoomCamLookat.cs
+++ Assets/_Script/Map/MapCam/BossRoomCamLookat.cs
@@ -31,7 +31,7 @@ public class BossRoomCamLookat : MonoBehaviour
             }
             else
             {
-                CamManager.Instance.CurrentCam.m_Lens.FieldOfView = Mathf.Lerp(CamManager.Instance.CurrentCam.m_Lens.FieldOfView, orgFOV + Vector2.Distance(player.position, boss.position) / 2f, Time.deltaTime * 5f);
+                CamManager.Instance.CurrentCam.m_Lens.FieldOfView = Mathf.Lerp(CamManager.Instance.CurrentCam.m_Lens.FieldOfView, orgFOV + Vector2.Distance(player.position, boss.position), Time.deltaTime * 5f);
             }
         }
 
