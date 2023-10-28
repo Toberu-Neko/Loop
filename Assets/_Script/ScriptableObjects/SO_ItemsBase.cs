0ef717cd86af8a4d4000ec77f20038f1e0a5da62
謝清華 <uni.toberuneko@gmail.com>
2023-10-28 16:13:55 +0800

    UI素材包

--- Assets/_Script/ScriptableObjects/SO_ItemsBase.cs
+++ Assets/_Script/ScriptableObjects/SO_ItemsBase.cs
@@ -6,6 +6,7 @@ public class SO_ItemsBase : ScriptableObject
 {
     [Header("Base")]
     public string itemName = "Defult item name";
+    [TextArea(3, 10)]
     public string displayName = "Defult display name";
     [TextArea(3, 10)]
     public string itemDescription = "Defult Description";
